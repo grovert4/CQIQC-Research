569e78d2fe3d989301faec05da5a905ad354406f
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2023-08-14 15:56:32 -0400

    bash script

---
+++ Bilayer_SkX/ClusterCode/run_monolayer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --mail-user=andrew.hardy@mail.utoronto.ca  
+#SBATCH --mail-type=ALL
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=40
+#SBATCH --account=def-aparamek
+#SBATCH --time=1:45:00
+#SBATCH --mem-per-cpu=4000MB
+#SBATCH --output=/scratch/andykh/SLURMOutputs/slurm-%x-%j.txt
+module load julia/1.9.1
+
+srun julia SkX_MonoLayer_Run.jl
