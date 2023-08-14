3f0fc0776e8a0955173d8483d9eed03d52d21464
grovert4 <123444686+grovert4@users.noreply.github.com>
2023-08-14 12:14:45 -0400

    Add files via upload

---
+++ Bilayer SkX/ClusterCode/SkX-BiLayer Run.jl
@@ -0,0 +1,92 @@
+using SpinMC, LinearAlgebra, Plots
+include("functions.jl")
+
+#Unit Cell Construction
+a1 = (1.0 , 0.0, 0.0)  #-
+a2 = (-1/2 , sqrt(3)/2, 0.0)  #/
+a3 = (0.0, 0.0, 2.0)  #|
+UCglobal = UnitCell(a1, a2, a3)
+
+b1 = addBasisSite!(UCglobal, (0.0, 0.0, 0.0)) ##layer A (z = 0)
+b2 = addBasisSite!(UCglobal, (0.0, 0.0, 1.0)) ##layer A  (z = 1)
+
+#Parameters
+J2 = 0.0
+J1 = 1.0
+A_ion = 0.2
+H = 0.0
+J_ll = -0.2
+D = 0.25
+
+#Helpful Matrices
+I = [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0]
+Sz = [0.0 0.0 0.0; 0.0 0.0 0.0; 0.0 0.0 1.0]
+
+D1v = D .* [1, 0, 0]
+D2v = D .* [-1/2,sqrt(3)/2 , 0] 
+D3v = D .* [-1/2, -sqrt(3)/2, 0] 
+
+ExchangeD(v) = [0 -v[3] v[2]; v[3] 0 -v[1]; -v[2] v[1] 0]
+
+D1ex = ExchangeD(D1v)
+D2ex = ExchangeD(D2v)
+D3ex = ExchangeD(D3v)
+
+addInteraction!(UCglobal, b1, b2, -J_ll * Sz , (0,0,0))
+
+##Same layer interactions
+for i in 1:length(UCglobal.basis)
+    #1nd NN ferromagnetic interaction
+    addInteraction!(UCglobal, i, i, -J1 * I, (1,0,0))
+    addInteraction!(UCglobal, i, i, -J1 * I, (0,1,0))
+    addInteraction!(UCglobal, i, i, -J1 * I, (-1,-1,0))
+
+
+    ##onsite anisotropy
+    setInteractionOnsite!(UCglobal, i, A_ion * Sz)
+
+
+    ##DM interaction
+    addInteraction!(UCglobal, i, i, (-1)^(i+1) * D1ex, (1,0,0))
+    addInteraction!(UCglobal, i, i, (-1)^(i+1) * D2ex, (0,1,0)) #(0,1)
+    addInteraction!(UCglobal, i, i, (-1)^(i+1) * D3ex, (-1,-1,0)) #
+end
+
+
+Hs = union(collect(range(0.0,1.1,length = 36)))
+J2s = collect(range(0.0,-0.5,length = 36))
+
+# addInteraction!(UC, b1, b2, -J_ll * Sz, (0,0,-1))
+
+L = (24, 24, 1)
+
+UClocal0 = deepcopy(UCglobal)
+Lattice0 = Lattice(UClocal0, L)
+vertex=getVertex(Lattice0)
+
+for (j2idx, j2) in enumerate(J2s)
+   global J2 = j2
+   for (hidx,h) in enumerate(Hs)
+      
+      global H = h
+      UClocal = deepcopy(UCglobal)
+
+      for i in 1:length(UCglobal.basis)
+         #Add J2 2NN AF interaction 
+         addInteraction!(UClocal, i, i, -J2 * I, (-1,1,0))
+         addInteraction!(UClocal, i, i, -J2 * I, (1,2,0))
+         addInteraction!(UClocal, i, i, -J2 * I, (2,1,0))
+         
+         #Local Magnetic field
+         setField!(UClocal, i, [0,0,-H])
+      end
+
+      latticeLocal = Lattice(UClocal, L)
+
+      mc = runAnneal(69,680,latticeLocal,2500,250000,0.99, H, J2,"Official-Cluster-Run-1-BiLayer/H=$h,J2=$j2");
+      # DetailedMonoPlot(mc,mc.lattice,vertex)
+      # SkXnumberPhase[hidx, j2idx] = round(getSkyrmionNumber(0,mc.lattice,vertex),digits=1)
+   end
+end
+
+
---
+++ Bilayer SkX/ClusterCode/SkX-MonoLayer Run.jl
@@ -0,0 +1,78 @@
+using SpinMC, LinearAlgebra, Plots
+include("functions.jl")
+
+#Unit Cell Construction
+a1 = (1.0 , 0.0)  #-
+a2 = (-1/2 , sqrt(3)/2)  #/
+UCglobal = UnitCell(a1 , a2)
+
+b1 = addBasisSite!(UCglobal, (0.0, 0.0)) ##layer A (z = 0)
+
+#Parameters
+J1 = 1.0
+A_ion = 0.2
+D = 0.25
+J_ll = 0.0
+
+#Helpful Matrices
+I = [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0]
+Sz = [0.0 0.0 0.0; 0.0 0.0 0.0; 0.0 0.0 1.0]
+
+#DM Vector
+D1v =  D .* [1, 0, 0]
+D2v =  D .* [-1/2,sqrt(3)/2 , 0]
+D3v =  D .* [-1/2, -sqrt(3)/2, 0]
+Dex(v) = [0 -v[3] v[2]; v[3] 0 -v[1]; -v[2] v[1] 0]
+
+##Global ayer interactions
+
+#1nd NN ferromagnetic interaction
+addInteraction!(UCglobal, 1, 1, -J1 * I, (1,0))
+addInteraction!(UCglobal, 1, 1, -J1 * I, (0,1))
+addInteraction!(UCglobal, 1, 1, -J1 * I, (-1,-1))
+
+##onsite anisotropy
+setInteractionOnsite!(UCglobal, 1, A_ion * Sz)
+
+##DM interaction
+addInteraction!(UCglobal, 1, 1, Dex(D1v), (1,0))
+addInteraction!(UCglobal, 1, 1, Dex(D2v), (0,1)) #(0,1)
+addInteraction!(UCglobal, 1, 1, Dex(D3v), (-1,-1)) #
+
+L = (24, 24)
+
+
+Hs = union(collect(range(0.0,1.1,length = 36)))
+J2s = collect(range(0.0,-0.5,length = 36))
+
+UClocal0 = deepcopy(UCglobal)
+Lattice0 = Lattice(UClocal0, L)
+vertex=getVertex(Lattice0)
+
+SkXnumberPhase = zeros(length(Hs),length(J2s))
+
+for (j2idx, j2) in enumerate(J2s)
+   global J2 = j2
+   for (hidx,h) in enumerate(Hs)
+
+      global H = h
+      UClocal = deepcopy(UCglobal)
+
+      #Add J2 2NN AF interaction 
+      addInteraction!(UClocal, 1, 1, -J2 * I, (-1,1))
+      addInteraction!(UClocal, 1, 1, -J2 * I, (1,2))
+      addInteraction!(UClocal, 1, 1, -J2 * I, (2,1))
+      
+      #Local Magnetic field
+      setField!(UClocal, 1, [0,0,-H])
+
+      latticeLocal = Lattice(UClocal, L)
+
+      mc = runAnneal(69,680,latticeLocal,2500,250000,0.99, H, J2,outfile = "Official-Cluster-Run-1-MonoLayer/H=$h,J2=$j2");
+      # DetailedMonoPlot(mc,mc.lattice,vertex)
+      # SkXnumberPhase[hidx, j2idx] = round(getSkyrmionNumber(0,mc.lattice,vertex),digits=1)
+   end
+end
+
+
+
