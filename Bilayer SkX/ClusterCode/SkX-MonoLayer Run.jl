733c910f1f3eeab5e9f80f732917bf4a0b454793
grovert4 <123444686+grovert4@users.noreply.github.com>
2023-08-15 17:40:32 -0400

    Update SkX-MonoLayer Run.jl

--- Bilayer SkX/ClusterCode/SkX-MonoLayer Run.jl
+++ Bilayer SkX/ClusterCode/SkX-MonoLayer Run.jl
@@ -1,7 +1,8 @@
-using Pkg
-Pkg.add(url="https://github.com/grovert4/SpinMC_more_more.jl")
-using SpinMC_more_more, LinearAlgebra, Plots
+using SpinMC_more_more, LinearAlgebra, Plots, JSON
 include("functions.jl")
+
+
+inputFile = JSON.parsefile("inputParametersMonoLayer.json")
 
 #Unit Cell Construction
 a1 = (1.0 , 0.0)  #-
@@ -10,11 +11,9 @@ UCglobal = UnitCell(a1 , a2)
 
 b1 = addBasisSite!(UCglobal, (0.0, 0.0)) ##layer A (z = 0)
 
-#Parameters
-J1 = 1.0
-A_ion = 0.2
-D = 0.25
-J_ll = 0.0
+J1 = inputFile["J_1"]
+D = inputFile["D"]
+A_ion = inputFile["A_ion"]
 
 #Helpful Matrices
 I = [1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0]
@@ -41,17 +40,23 @@ addInteraction!(UCglobal, 1, 1, Dex(D1v), (1,0))
 addInteraction!(UCglobal, 1, 1, Dex(D2v), (0,1)) #(0,1)
 addInteraction!(UCglobal, 1, 1, Dex(D3v), (-1,-1)) #
 
-L = (24, 24)
+L = (inputFile["System_Size"], inputFile["System_Size"])
 
 
-Hs = union(collect(range(0.0,1.1,length = 36)))
-J2s = collect(range(0.0,-0.5,length = 36))
+Hs = union(collect(range(inputFile["H_min"], inputFile["H_max"],length = inputFile["H_length"])))
+J2s = collect(range(inputFile["J2_max"],inputFile["J2_min"],length = inputFile["J2_length"]))
 
 UClocal0 = deepcopy(UCglobal)
 Lattice0 = Lattice(UClocal0, L)
 vertex=getVertex(Lattice0)
 
 SkXnumberPhase = zeros(length(Hs),length(J2s))
+
+t0 = inputFile["t_max"]
+tf = inputFile["t_min"]
+coolRate = inputFile["coolRate"]
+thermalizationSweeps = inputFile["thermalizationSweeps"]
+measurementSweeps = inputFile["measurementSweeps"]
 
 for (j2idx, j2) in enumerate(J2s)
    global J2 = j2
@@ -70,7 +75,7 @@ for (j2idx, j2) in enumerate(J2s)
 
       latticeLocal = Lattice(UClocal, L)
 
-      mc = runAnneal(69,680,latticeLocal,2500,250000,0.99, H, J2,"Official-Cluster-Run-1-MonoLayer/H=$h,J2=$j2");
+      mc = runAnneal(t0,tf,latticeLocal,thermalizationSweeps,measurementSweeps,coolRate, H, J2,"Official-Cluster-Run-1-MonoLayer/H=$h,J2=$j2");
       # DetailedMonoPlot(mc,mc.lattice,vertex)
       # SkXnumberPhase[hidx, j2idx] = round(getSkyrmionNumber(0,mc.lattice,vertex),digits=1)
    end
