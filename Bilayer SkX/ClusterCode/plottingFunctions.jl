332eab076a24ef6990b35f2c54add258e0386a5f
grovert4 <123444686+grovert4@users.noreply.github.com>
2023-08-23 10:53:26 -0400

    Update plottingFunctions.jl

--- Bilayer SkX/ClusterCode/plottingFunctions.jl
+++ Bilayer SkX/ClusterCode/plottingFunctions.jl
@@ -538,3 +538,88 @@ function plotSx(lat::Lattice)
         return f1
     end
 end
+
+function plotReadSpins(file, lat, vertex)
+    updateSpins!(file,lat)
+    PyPlot.isjulia_display[] = false  
+    xpos,ypos = [],[]
+ 
+    for i in 1:length(lat.sitePositions)
+        push!(xpos, lat.sitePositions[i][1])
+        push!(ypos, lat.sitePositions[i][2])
+    end
+    xspin1,yspin1,zspin1 = [],[],[]
+    for i in 1:length(lat.spins[1,:])
+        push!(xspin1,lat.spins[1,i])
+        push!(yspin1,lat.spins[2,i])
+        push!(zspin1,lat.spins[3,i])
+    end
+    len1 = sqrt.(xspin1.^2  + yspin1.^2)
+    max1 = maximum(len1)
+    min1 = minimum(len1) 
+    s1 = ((len1 .- min1) .* ((1 - 0.4)/(max1 - min1)) .+ 0.4) ./ len1
+    xspin1 = @. xspin1 .* s1
+    yspin1 = @. yspin1 .* s1
+ 
+    PyPlot.close("all")
+ 
+    PyPlot.rc("xtick", labelsize=8)
+    PyPlot.rc("ytick", labelsize=8)
+ 
+    sizer = (lat.size[1],lat.size[2]) 
+ 
+    PyPlot.isjulia_display[] = false
+    f1 = PyPlot.figure(dpi=250)
+    title(L"J_1 = %$J1, J_2 = %$J2, H = %$H, D = %$D, A = %$A_ion")
+    pcolor(reshape(xpos,sizer),reshape(ypos,sizer), reshape(zspin1,sizer),vmin = -1, vmax = 1,cmap = "bwr", alpha=0.6) 
+    colorbar(shrink = 0.585, pad=0.01)
+    p1 = PyPlot.quiver(xpos,ypos, xspin1,yspin1, width = 0.00325, headlength = 4,headaxislength = 3.5,angles="xy", scale_units="xy", scale=1)
+    ax1 = PyPlot.gca()            
+    ax1.set_aspect("equal","box")
+    f1.tight_layout(pad=-2.5)
+ 
+    if length(vertex) != 0
+       skxnum0 = round(getSkyrmionNumber(0, lat, vertex), sigdigits=3)
+       chirality0 = round(getScalarChirality(0,lat,vertex),sigdigits=3)
+       energy, std = readEnergy(file)
+       energy = round(energy, sigdigits=4)
+       std = round(std, sigdigits=3)
+       # sf1 = StructureFactor(mc, 0)
+       # maxvalsidx1 = partialsortperm(vec(sf1), 1:3,rev=true)
+       # maxvals1 = round.(partialsort(vec(sf1), 1:3,rev=true),sigdigits=3)
+       # z11 = maxvals1[1]
+       # z12 = maxvals1[2]
+       # z13 = maxvals1[3]
+       # kx = collect(range(-5pi/3,5pi/3,length=256))
+       # ky = collect(range(-5pi/3,5pi/3, length=256))
+       # qxs1 = round.(kx[Int64.(ceil.(maxvalsidx1 ./ 256))],sigdigits=3)
+       # x11 = qxs1[1]
+       # x12 = qxs1[2]
+       # x13 = qxs1[3] 
+       # qys1 = round.(ky[(maxvalsidx1 .% 256)],sigdigits=3) 
+       # y11 = qys1[1]
+       # y12 = qys1[2]
+       # y13 = qys1[3]
+ 
+       annotate(L"SkX Number: %$skxnum0, Chirality: %$chirality0, Energy: %$energy \pm %$std,", xy=(0.5, -0.1), xycoords="axes fraction", ha = "center", fontsize=9)
+       # annotate(L"SF Peak: (%$x11, %$y11): %$z11", xy=(0.5, -0.15), xycoords="axes fraction", ha = "center", fontsize=9)
+   end
+ 
+    display(f1)
+ 
+end
+
+function plotSzLine(lat, siteInit, factor)
+    site = siteInit
+    ind = 1
+    v = lat.sitePositions[siteInit + face] .- lat.sitePositions[siteInit]
+    xspin1,yspin1,zspin1 = [],[],[]
+    while checkbounds(Bool, tempLattice.sitePositions, site)
+        push!(xspin1,lat.spins[1,site])
+        push!(yspin1,lat.spins[2,site])
+        push!(zspin1,lat.spins[3,site])
+        site += factor
+        ind += 1
+    end
+    Plots.quiver(collect(1:length(xspin1)), repeat([0],length(xspin1)),quiver=(xspin1,zspin1), legend=false)
+end
