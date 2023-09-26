45f5f9965b047a59410a07e94eb579c75f68955a
grovert4 <123444686+grovert4@users.noreply.github.com>
2023-09-26 17:55:57 -0400

    Update Monolayer-Tightbinding.jl

--- Bilayer SkX/TightBindingStuff/Monolayer-Tightbinding.jl
+++ Bilayer SkX/TightBindingStuff/Monolayer-Tightbinding.jl
@@ -1,8 +1,5 @@
 using Plots, TightBindingToolkit, LinearAlgebra, ColorSchemes
 
-#Skyrmion Lattice
-# a1 = [3.0 , sqrt(3)]
-# a2 = [3.0 , -sqrt(3)]
 a1 = [-3.0, sqrt(3)]
 a2 = [3.0, sqrt(3)]
 UC = UnitCell([a1 , a2] , 2) 
@@ -13,8 +10,8 @@ l2 = [-0.5, sqrt(3)/2]
 
 ##Parameters
 t = 1.0
-jh = -20.0
 pspin = SpinMats(1//2)
+JH = 1.0
 
 
 ##Adding inner-hexagon structure  
@@ -31,8 +28,8 @@ AddIsotropicBonds!(UC, 1.0, -t * pspin[4], "iso", checkOffsetRange = 1)
 ep = 1.0
 tau1(v) = [sin(pi * (1 - norm(v)/2)) * v[1]/norm(v), sin(pi * (1 - norm(v)/2 )) * v[2]/norm(v), cos(pi * (1 - norm(v)/2 ))]
 tau2(v) = [0,0,1]
-tau(v) = ep .* tau1(v) .+ (1 - ep) .* tau2(v)
-# tau(v) = tau1(v)
+# tau(v) = ep .* tau1(v) .+ (1 - ep) .* tau2(v)
+tau(v) = tau1(v)
 sigmav(i,j) = 2 .* [pspin[1][i,j], pspin[2][i,j], pspin[3][i,j]]
 s11 = sigmav(1,1)
 s12 = sigmav(1,2)
@@ -47,36 +44,36 @@ for (ind, bas) in enumerate(UC.basis)
         mat = intermat(normalize(tau(bas) + tau(-bas))) 
     else 
         closest = [bas, bas-a1, bas-a2]
-        mat = intermat(tau( closest[findmin(x -> norm(x), closest)[2]] ))
+        spn = tau( closest[findmin(x -> norm(x), closest)[2]] )
+        replace!(spn, NaN=> 0.0)
+        mat = intermat(spn)
     end
-    replace!(mat, NaN + NaN * im=> 0.0 + 0.0 * im)
-    AddAnisotropicBond!(UC, ind, ind, [0,0], -jh * mat, 0.0, "interaction")
+    AddAnisotropicBond!(UC, ind, ind, [0,0], -JH * mat, 0.0, "interaction")
 end
 
 ##Plotting the unit cell
-plot_UC = Plot_UnitCell!(UC);
+# plot_UC = Plot_UnitCell!(UC);
 # display(plot_UC)
 
 ##Creating BZ and Hamiltonian Model
-const kSize = 6 * 15 + 3  
+kSize = 6 * 10 + 3  
 bz = BZ(kSize)
 FillBZ!(bz, UC)
 path = CombinedBZPath(bz, [bz.HighSymPoints["G"], bz.HighSymPoints["K1"], bz.HighSymPoints["M2"]] ; nearest=true)
 H = Hamiltonian(UC, bz)
 DiagonalizeHamiltonian!(H)
-Mdl = Model(UC, bz, H)
-SolveModel!(Mdl)
+Mdl = Model(UC, bz, H; filling = 0.5)
+SolveModel!(Mdl; get_gap=true)
 
 ##Plotting the band structure
 bands = Plot_Band_Structure!(Mdl, [bz.HighSymPoints["G"], bz.HighSymPoints["K1"], bz.HighSymPoints["M2"]] , labels = ["G", "K1", "M2"], plot_legend=false);
 # plot!(bands, legend = false);
-# display(bands)
+display(bands)
 
-##Calculating Chern Numbers for bands
-# for i in 1:2*length(UC.basis)
-#     c = ChernNumber(H, [i])
-#     println(round(c))
-# end
+#Calculating Chern Numbers for bands
+for i in 1:2*length(UC.basis)
+    c = ChernNumber(H, [i])
+    println(round(c))
+end
 println("Chern Number for first 12 bands: ", ChernNumber(H, collect(1:12)))
-println("Chern Number for last 12 bands: ", ChernNumber(H, collect(13:24)))
-println("Overall Chern Number", ChernNumber(H, collect(1:24)))
+
