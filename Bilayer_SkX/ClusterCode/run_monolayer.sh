50dcede94a7f2090e3324e759fca058d107f0160
grovert4 <123444686+grovert4@users.noreply.github.com>
2023-10-14 20:50:44 -0400

    Update run_monolayer.sh

--- Bilayer_SkX/ClusterCode/run_monolayer.sh
+++ Bilayer_SkX/ClusterCode/run_monolayer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --mail-user=andrew.hardy@mail.utoronto.ca  
+#SBATCH --mail-user=tanmay.grover@mail.utoronto.ca  
 #SBATCH --mail-type=ALL
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
@@ -7,9 +7,9 @@
 #SBATCH --ntasks=90
 #SBATCH --cpus-per-task=1 
 #SBATCH --account=def-aparamek
-#SBATCH --time=2:45:00
+#SBATCH --time=4:00:00
 #SBATCH --mem-per-cpu=4000MB
-#SBATCH --output=/scratch/andykh/SLURMOutputs/slurm-%x-%j.txt
+#SBATCH --output=/scratch/grovert4/SLURM/slurm-%x-%j.txt
 module load julia/1.8.5
 
-srun julia SkX_MonoLayer_Run.jl 08.21.2023_Monolayer
+srun julia SkX_MonoLayer_Run.jl 10.14.2023-Monolayer
