2e101d2b747e7e97f787c273fba19b4c64e0f691
grovert4 <123444686+grovert4@users.noreply.github.com>
2023-10-30 19:17:14 -0400

    Update run_bilayer.sh

--- Bilayer_SkX/ClusterCode/run_bilayer.sh
+++ Bilayer_SkX/ClusterCode/run_bilayer.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=90
 #SBATCH --cpus-per-task=1 
 #SBATCH --account=def-aparamek
-#SBATCH --time=6:00:00
+#SBATCH --time=10:00:00
 #SBATCH --mem-per-cpu=4000MB
 #SBATCH --output=/scratch/grovert4/SLURM/slurm-%x-%j.txt
 module load julia/1.8.5
