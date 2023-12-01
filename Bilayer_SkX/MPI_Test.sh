c0c9ac20d6ff305ba079534752f2efa28a167936
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2023-12-01 11:59:08 -0500

    ugh

--- Bilayer_SkX/MPI_Test.sh
+++ Bilayer_SkX/MPI_Test.sh
@@ -3,5 +3,6 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=0:45:00
 #SBATCH --output=/scratch/andykh/SLURMOutputs/slurm-%x-%j.txt
-module load julia/1.8.5
+module load julia/1.9.1
+module load NiaEnv/2022a
 srun julia MPITest.jl
