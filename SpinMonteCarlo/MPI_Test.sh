be27ea03291c0b9bbc4594e3e8719dea22440200
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2023-12-02 17:27:44 -0500

    too many directories

--- SpinMonteCarlo/MPI_Test.sh
+++ SpinMonteCarlo/MPI_Test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --mail-user=andrew.hardy@mail.utoronto.ca  
+#SBATCH --mail-user=andrew.hardy@mail.utoronto.ca
 #SBATCH --mail-type=ALL
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 #SBATCH --ntasks=40
-#SBATCH --cpus-per-task=1 
+#SBATCH --cpus-per-task=1
 #SBATCH --account=rrg-aparamek
 #SBATCH --time=10:00:00
 #SBATCH --output=/scratch/a/aparamek/andykh/SLURMOutputs/slurm-%x-%j.txt
