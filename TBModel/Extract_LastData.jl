41c03e580c1fb795fe252b4e3de984c00e6028cc
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2023-12-08 17:44:03 -0500

    Data

--- TBModel/Extract_LastData.jl
+++ TBModel/Extract_LastData.jl
@@ -5,20 +5,23 @@ function extract_data!(folderpath::String, substring::String=".jld2")
     file_list = sort(readdir(folderpath))
     for file in file_list
         if occursin(substring,string(file))
-        try
-            data_entry = load(folderpath * "/" * string(file))
-            jldopen(folderpath * "/LastItr" *  "/" * "Last_Itr_" * file , "w" ) do dict 
-                dict["Iterations"] = data_entry["Self-consistency params"][:iter]
-                dict["Convergence"] = [maximum(norm.(data_entry["outputs"][i] - data_entry["inputs"][i])) for i in 1:length(data_entry["inputs"])]
-                dict["MFT Energy"] = data_entry["function args"][1].MFTEnergy
-                dict["Pairing Block"] = Lookup(data_entry["function args"][1].PairingOrders)
-                dict["Hopping Block"] = Lookup(data_entry["function args"][1].HoppingOrders)
-                dict["Hopping UC"] = data_entry["function args"][1].model.uc_hop
-                dict["Gr"] = data_entry["function args"][1].model.Gr
-                dict["outputs"] = data_entry["outputs"][end]
-            end
-        catch e
-        println("Error Loading $file")
+            println("TRYING TO LOAD "*folderpath * "/" * string(file))
+            try
+                println("TRYING TO LOAD "*folderpath * "/" * string(file))
+
+                data_entry = load(folderpath * "/" * string(file))
+                jldopen(folderpath * "/LastItr/Last_Itr_" * file , "w" ) do dict 
+                    dict["Iterations"] = data_entry["Self-consistency params"][:iter]
+                    dict["Convergence"] = [maximum(norm.(data_entry["outputs"][i] - data_entry["inputs"][i])) for i in 1:length(data_entry["inputs"])]
+                    dict["MFT Energy"] = data_entry["function args"][1].MFTEnergy
+                    dict["Pairing Block"] = Lookup(data_entry["function args"][1].PairingOrders)
+                    dict["Hopping Block"] = Lookup(data_entry["function args"][1].HoppingOrders)
+                    dict["Hopping UC"] = data_entry["function args"][1].model.uc_hop
+                    dict["Gr"] = data_entry["function args"][1].model.Gr
+                    dict["outputs"] = data_entry["outputs"][end]
+                end
+            catch e
+                println("Error Loading $file")
         end
         end
     end
