c9525783b5550c23352ae1890f75ed4c2edbf940
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2023-12-22 13:59:47 -0500

    checking

--- TBModel/Extract_LastData.jl
+++ TBModel/Extract_LastData.jl
@@ -5,39 +5,44 @@ function extract_data!(folderpath::String, substring::String=".jld2")
     file_list = sort(readdir(folderpath))
     for file in file_list
         if occursin(substring, string(file))
-            try
-                println("TRYING TO LOAD " * folderpath * "/" * string(file))
-                data_entry = load(folderpath * "/" * string(file))
-                println("SUCCESFULLY LOADED " * folderpath * "/" * string(file))
-                dict = Dict()
-                dict["Iterations"] = data_entry["Self-consistency params"][:iter]
-                dict["MFT Energy"] = data_entry["function args"][1].MFTEnergy
-                dict["Hopping Block"] = data_entry["function args"][1].HoppingOrders
-                TBModel = data_entry["function args"][1].model
-                path = [TBModel.bz.HighSymPoints["G"], TBModel.bz.HighSymPoints["M2"], TBModel.bz.HighSymPoints["M3"]]
-                bzpath     = CombinedBZPath(TBModel.bz, path ; nearest = true, closed = true)
-                path_index = GetQIndex.(bzpath, Ref(TBModel.bz) ; nearest = true)
-                bands_from_index = getindex.(Ref(TBModel.Ham.bands), CartesianIndex.(Tuple.(path_index)))
-                label_indices    = getindex.(findmin.([norm.(Ref(ReduceQ(x,TBModel.bz)).-bzpath) for x in path]) , 2)
-                #Can you save these data arrays? 
-                c = Array{Float32}(undef, 2 * length(TBModel.uc.basis))
-                for i in 1:2*length(TBModel.uc.basis)
-                    c[i] = ChernNumber(TBModel.Ham, collect(1:i))
-                    println(round(c[i]), "Chern")
-                    c_fill = FilledChernNumber(TBModel.Ham, TBModel.mu)
-                end
-                dict["Bands"] = bands_from_index
-                dict["Labels"] = label_indices
-                dict["UC"]  = TBModel.uc
-                dict["Gap"] = TBModel.gap # I seem to have done this wrong? 
-                dict["mu"]  = TBModel.mu
-                dict["Outputs"] = data_entry["outputs"][end]
-                #plot = Plot_Band_Structure!(TBModel, [TBModel.bz.HighSymPoints["G"], TBModel.bz.HighSymPoints["M2"], TBModel.bz.HighSymPoints["M3"]]; labels=[L"\Gamma", L"M_2", L"M_3"])
+            if isfile(folderpath * "/Last_Itr/Last_Itr_" * string(file))
+                println("FILE EXISTS : " *folderpath * "/Last_Itr/Last_Itr_" * string(file))
+                continue
+            else
+                try
+                    println("TRYING TO LOAD " * folderpath * "/" * string(file))
+                    data_entry = load(folderpath * "/" * string(file))
+                    println("SUCCESFULLY LOADED " * folderpath * "/" * string(file))
+                    dict = Dict()
+                    dict["Iterations"] = data_entry["Self-consistency params"][:iter]
+                    dict["MFT Energy"] = data_entry["function args"][1].MFTEnergy
+                    dict["Hopping Block"] = data_entry["function args"][1].HoppingOrders
+                    TBModel = data_entry["function args"][1].model
+                    path = [TBModel.bz.HighSymPoints["G"], TBModel.bz.HighSymPoints["M2"], TBModel.bz.HighSymPoints["M3"]]
+                    bzpath     = CombinedBZPath(TBModel.bz, path ; nearest = true, closed = true)
+                    path_index = GetQIndex.(bzpath, Ref(TBModel.bz) ; nearest = true)
+                    bands_from_index = getindex.(Ref(TBModel.Ham.bands), CartesianIndex.(Tuple.(path_index)))
+                    label_indices    = getindex.(findmin.([norm.(Ref(ReduceQ(x,TBModel.bz)).-bzpath) for x in path]) , 2)
+                    #Can you save these data arrays? 
+                    c = Array{Float32}(undef, 2 * length(TBModel.uc.basis))
+                    for i in 1:2*length(TBModel.uc.basis)
+                        c[i] = ChernNumber(TBModel.Ham, collect(1:i))
+                        println(round(c[i]), "Chern")
+                        c_fill = FilledChernNumber(TBModel.Ham, TBModel.mu)
+                    end
+                    dict["Bands"] = bands_from_index
+                    dict["Labels"] = label_indices
+                    dict["UC"]  = TBModel.uc
+                    dict["Gap"] = TBModel.gap # I seem to have done this wrong? 
+                    dict["mu"]  = TBModel.mu
+                    dict["Outputs"] = data_entry["outputs"][end]
+                    #plot = Plot_Band_Structure!(TBModel, [TBModel.bz.HighSymPoints["G"], TBModel.bz.HighSymPoints["M2"], TBModel.bz.HighSymPoints["M3"]]; labels=[L"\Gamma", L"M_2", L"M_3"])
 
-                dict["Chern"] = c
-                save(folderpath * "/Last_Itr/Last_Itr_" * string(file), dict)
-            catch e
-                println("Error Loading $file")
+                    dict["Chern"] = c
+                    save(folderpath * "/Last_Itr/Last_Itr_" * string(file), dict)
+                catch e
+                    println("Error Loading $file")
+                end
             end
         end
     end
