2e4a0461d4149c89e05f2145d24ea59a47d7e569
grovert4 <123444686+grovert4@users.noreply.github.com>
2024-01-14 14:22:53 -0500

    Create singlerun.sh

---
+++ Bilayer_SkX/ClusterCode/singlerun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --mail-user=tanmay.grover@mail.utoronto.ca  
+#SBATCH --mail-type=ALL
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=1 
+#SBATCH --account=def-aparamek
+#SBATCH --time=1:00:00
+#SBATCH --mem-per-cpu=4000MB
+#SBATCH --output=/scratch/grovert4/SLURM/slurm-%x-%j.txt
+module load julia/1.8.5
+
+srun julia Single_Run_Monolayer.jl 13.01.2023-Monolayer
