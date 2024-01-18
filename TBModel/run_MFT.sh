cc3c1a55dbcba40606ebd4d9bd7d9c634ac4f96f
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2024-01-18 14:05:44 -0500

    trying new run

--- TBModel/run_MFT.sh
+++ TBModel/run_MFT.sh
@@ -4,12 +4,12 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --ntasks=20
-#SBATCH --cpus-per-task=2
+#SBATCH --ntasks=40
+#SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
 #SBATCH --account=rrg-aparamek
-#SBATCH --time=6:00:00
+#SBATCH --time=23:00:00
 #SBATCH --output=/scratch/a/aparamek/andykh/SLURMOutputs/slurm-%x-%j.txt
 module load julia/1.9.1
 module load intelmpi
-mpirun julia --heap-size-hint=3G MFT_wrapper.jl 11.27.2023_Bilayer
+mpirun julia --heap-size-hint=3G MFT_wrapper.jl 01.15.2024_Bilayer
