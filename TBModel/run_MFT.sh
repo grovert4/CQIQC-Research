19fe08ee606df97ef0d9a782945422155e404e65
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2024-01-18 14:28:34 -0500

    data

--- TBModel/run_MFT.sh
+++ TBModel/run_MFT.sh
@@ -10,6 +10,8 @@
 #SBATCH --account=rrg-aparamek
 #SBATCH --time=23:00:00
 #SBATCH --output=/scratch/a/aparamek/andykh/SLURMOutputs/slurm-%x-%j.txt
+module load CCEnv
+module load StdEnv
 module load julia/1.9.1
 module load intelmpi
 mpirun julia --heap-size-hint=3G MFT_wrapper.jl 01.15.2024_Bilayer
