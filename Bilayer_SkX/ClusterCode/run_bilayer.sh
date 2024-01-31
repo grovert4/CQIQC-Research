fa03c881c0845d54427388d9d2fd72dd86d312f8
grovert4 <123444686+grovert4@users.noreply.github.com>
2024-01-31 18:26:50 -0500

    Update run_bilayer.sh

--- Bilayer_SkX/ClusterCode/run_bilayer.sh
+++ Bilayer_SkX/ClusterCode/run_bilayer.sh
@@ -4,12 +4,12 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --ntasks=90
+#SBATCH --ntasks=150
 #SBATCH --cpus-per-task=1 
 #SBATCH --account=def-aparamek
-#SBATCH --time=10:00:00
+#SBATCH --time=08:00:00
 #SBATCH --mem-per-cpu=4000MB
 #SBATCH --output=/scratch/grovert4/SLURM/slurm-%x-%j.txt
 module load julia/1.8.5
 
-srun julia SkX_BiLayer_Run.jl 17.10.2023-Bilayer
+srun julia SkX_BiLayer_Run.jl 30.01.2024-Bilayer
