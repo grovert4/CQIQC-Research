4fadc584ef98dd6c0db92ca2a9a1f2be2cc2497c
grovert4 <123444686+grovert4@users.noreply.github.com>
2024-02-15 12:14:39 -0500

    Update SkX_BiLayer_Run.jl

--- Bilayer_SkX/ClusterCode/SkX_BiLayer_Run.jl
+++ Bilayer_SkX/ClusterCode/SkX_BiLayer_Run.jl
@@ -74,7 +74,7 @@ for i in start_index:end_index
    j2 = round(J2s[i],sigdigits=5)
 
    #println("Rank " , commRank , " working on h = " , h, " working on j2 = ", j2) 
-   filename = "/scratch/grovert4/Data/partial_bilayer/"*ARGS[1]*"_Jperp=$jperp,J2=$j2.h5"
+   filename = "/scratch/grovert4/Data/partial_bilayer/nofield"*ARGS[1]*"_Jperp=$jperp,J2=$j2.h5"
    #println(filename)
    if isfile(filename) 
       println("Already Completed "*filename)
