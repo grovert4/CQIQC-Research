ccce707d0d68ece4180ccb26df48105f57187046
grovert4 <123444686+grovert4@users.noreply.github.com>
2024-03-04 17:43:12 -0500

    Create run_secondphase.sh

---
+++ Bilayer_SkX/ClusterCode/run_secondphase.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --mail-user=tanmay.grover@mail.utoronto.ca  
+#SBATCH --mail-type=ALL
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --ntasks=150
+#SBATCH --cpus-per-task=1 
+#SBATCH --account=def-aparamek
+#SBATCH --time=6:00:00
+#SBATCH --mem-per-cpu=4000MB
+#SBATCH --output=/scratch/grovert4/SLURM/slurm-%x-%j.txt
+module load julia/1.8.5
+
+srun julia PhaseDiagram2.jl 03.03.2024-Bilayer-nofield
