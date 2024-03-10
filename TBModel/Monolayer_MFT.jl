9cde185e0fe64694b835e22f11068916ad7abcfb
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2024-03-10 16:39:54 -0400

    trying monolayer

--- TBModel/Monolayer_MFT.jl
+++ TBModel/Monolayer_MFT.jl
@@ -91,7 +91,7 @@ function MFT(params, filename)
     # add filename to input 
     fileName = loc * "/$(filename)_p=$(round(filling, digits=3))_U=$(round(U, digits=2))_t1=$(round(t1, digits=2)).jld2"
     GC.gc()
-    init_guess = fill(0.25, 12)
+    init_guess = fill(0.10, 25)
     if isfile(fileName)
         println("TRYING TO LOAD " * fileName)
         try
