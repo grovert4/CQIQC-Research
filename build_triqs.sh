e3796087a6e2428338b2a3c40aba1705bc14a920
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2024-04-09 19:49:19 -0400

    fk

--- build_triqs.sh
+++ build_triqs.sh
@@ -4,46 +4,17 @@ MODULES=" CCEnv StdEnv/2023 gcc flexiblas openmpi cmake fftw  hdf5 boost python/
 module purge
 module load ${MODULES}
 
-# export CC=clang
-# export CXX=clang++
-# export CFLAGS="-march=broadwell"
-# export CXXFLAGS="-stdlib=libc++ -Wno-register -march=broadwell"
-# export FC=gfortran
 
-# export BLA_VENDOR=FlexiBLAS
-
-# # set up MKL / OpenMP:
-# export MKL_INTERFACE_LAYER=GNU,LP64
-# export MKL_THREADING_LAYER=SEQUENTIAL
-# export MKL_NUM_THREADS=1
-# export OMP_NUM_THREADS=1
-
-# # set number of threads for compiling and testing
-# NCORES=10
-
-# BUILDDIR=$(pwd)
-# # set installation directory (default pwd/install)
-# INSTALLDIR=$(pwd)/install
-
-# export TRIQS_ROOT=${INSTALLDIR}
-# export PATH=${INSTALLDIR}/bin:$PATH
-# export CPLUS_INCLUDE_PATH=${INSTALLDIR}/include:$CPLUS_INCLUDE_PATH
-# export LIBRARY_PATH=${INSTALLDIR}/lib:$LIBRARY_PATH
-# export LD_LIBRARY_PATH=${INSTALLDIR}/lib:$LD_LIBRARY_PATH
-# export PYTHONPATH=${INSTALLDIR}/lib/python3.10/site-packages:$PYTHONPATH
-# export CMAKE_PREFIX_PATH=${INSTALLDIR}/lib/cmake/triqs:$CMAKE_PREFIX_PATH
-# export CMAKE_PREFIX_PATH=${INSTALLDIR}/lib/cmake/cpp2py:$CMAKE_PREFIX_PATH
-# choose triqs + app to be installed
 export CC=clang
 export CXX=clang++
-export FC=gfortran
-#python version detection
+export CFLAGS="-march=broadwell"
 export PYVER=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+export CXXFLAGS="-stdlib=libc++ -Wno-register -march=broadwell"
+export FC=gfortran
 # compiler flags add stdlib=libc++ for clang
-export CXXFLAGS="-stdlib=libc++ -Wno-register -march=native"
 
 # set blas / lapack Intel10_64_dyn | OpenBLAS | FlexiBLAS
-export BLA_VENDOR=Intel10_64_dyn
+export BLA_VENDOR=FlexiBLAS
 
 # set up MKL / OpenMP:
 export MKL_INTERFACE_LAYER=GNU,LP64
@@ -65,7 +36,7 @@ export LIBRARY_PATH=${INSTALLDIR}/lib:$LIBRARY_PATH
 export LD_LIBRARY_PATH=${INSTALLDIR}/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=${INSTALLDIR}/lib/python${PYVER}/site-packages:$PYTHONPATH
 export CMAKE_PREFIX_PATH=${INSTALLDIR}/lib/cmake/triqs:${INSTALLDIR}/lib/cmake/cpp2py:$CMAKE_PREFIX_PATH
-packages="triqs tprf"
+packages="triqs"
 
 
 for pkg in ${packages} ; do 
@@ -74,7 +45,7 @@ for pkg in ${packages} ; do
     # fetch latest changes
     cd $pkg.src && git pull
     mkdir -p build && cd build
-    cmake ../ -DCMAKE_INSTALL_PREFIX=${INSTALLDIR} -DMPIEXEC_PREFLAGS='--allow-run-as-root'
+    cmake ../$pkg.src -DCMAKE_INSTALL_PREFIX=${INSTALLDIR} -DMPIEXEC_PREFLAGS='--allow-run-as-root'
     make -j$NCORES
     # some test may use mpi
     ctest -j1 2>&1 >> ${testlog}
