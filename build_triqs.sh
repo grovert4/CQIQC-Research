ba8b9f6acf46f9e484a78776a0ddf2837793c216
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2024-04-09 19:08:44 -0400

    new script

--- build_triqs.sh
+++ build_triqs.sh
@@ -1,10 +1,46 @@
+# #!/bin/bash
+
+# # load modules
+# MODULES="CCEnv StdEnv/2023 gcc/12.3 flexiblas/3.3.1 openmpi/4.1.5 cmake/3.27.7 fftw/3.3.10 hdf5-mpi/1.14.2 boost/1.82.0 python/3.10.13 mpi4py/3.1.4 imkl/2023.2.0 llvm/16.0.6 eigen/3.4.0"
+# module purge
+# module load ${MODULES}
+# # choose your compilers here
+# BUILDDIR=$(pwd)
+# # set installation directory (default pwd/install)
+# INSTALLDIR=$(pwd)/install
+
+# export TRIQS_ROOT=${INSTALLDIR}
+# export PATH=${INSTALLDIR}/bin:$PATH
+# export CPLUS_INCLUDE_PATH=${INSTALLDIR}/include:$CPLUS_INCLUDE_PATH
+# export LIBRARY_PATH=${INSTALLDIR}/lib:$LIBRARY_PATH
+# export LD_LIBRARY_PATH=${INSTALLDIR}/lib:$LD_LIBRARY_PATH
+# export PYTHONPATH=${INSTALLDIR}/lib/python${PYVER}/site-packages:$PYTHONPATH
+# export CMAKE_PREFIX_PATH=${INSTALLDIR}/lib/cmake/triqs:${INSTALLDIR}/lib/cmake/cpp2py:$CMAKE_PREFIX_PATH
+
+# # output log files
 #!/bin/bash
 
-# load modules
-MODULES="CCEnv StdEnv/2023 gcc/12.3 flexiblas/3.3.1 openmpi/4.1.5 cmake/3.27.7 fftw/3.3.10 hdf5-mpi/1.14.2 boost/1.82.0 python/3.10.13 mpi4py/3.1.4 imkl/2023.2.0 llvm/16.0.6 eigen/3.4.0"
-module purge
-module load ${MODULES}
 # choose your compilers here
+export CC=clang
+export CXX=clang++
+export FC=gfortran
+#python version detection
+export PYVER=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+# compiler flags add stdlib=libc++ for clang
+export CXXFLAGS="-stdlib=libc++ -Wno-register -march=native"
+
+# set blas / lapack Intel10_64_dyn | OpenBLAS | FlexiBLAS
+export BLA_VENDOR=Intel10_64_dyn
+
+# set up MKL / OpenMP:
+export MKL_INTERFACE_LAYER=GNU,LP64
+export MKL_THREADING_LAYER=SEQUENTIAL
+export MKL_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+
+# set number of threads for compiling and testing
+NCORES=10
+
 BUILDDIR=$(pwd)
 # set installation directory (default pwd/install)
 INSTALLDIR=$(pwd)/install
@@ -16,9 +52,6 @@ export LIBRARY_PATH=${INSTALLDIR}/lib:$LIBRARY_PATH
 export LD_LIBRARY_PATH=${INSTALLDIR}/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=${INSTALLDIR}/lib/python${PYVER}/site-packages:$PYTHONPATH
 export CMAKE_PREFIX_PATH=${INSTALLDIR}/lib/cmake/triqs:${INSTALLDIR}/lib/cmake/cpp2py:$CMAKE_PREFIX_PATH
-
-# output log files
-
 
 # choose triqs + app to be installed
 packages="triqs  tprf "
