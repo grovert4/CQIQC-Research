0cecbd2fd1dbc2558928e6cfc695f4259d6afb2a
Andrew Hardy <andrewkoehlerhardy@gmail.com>
2024-04-09 19:38:00 -0400

    new script

--- build_triqs.sh
+++ build_triqs.sh
@@ -1,16 +1,49 @@
 # #!/bin/bash
 
-MODULES=" CCEnv StdEnv2023 gcc flexiblas openmpi cmake fftw  hdf5 boost python/3.10.13 llvm/16 eigen mpfr clang"
+MODULES=" CCEnv StdEnv/2023 gcc flexiblas openmpi cmake fftw  hdf5 boost python/3.10.13 llvm/16 eigen mpfr clang"
 module purge
 module load ${MODULES}
 
+# export CC=clang
+# export CXX=clang++
+# export CFLAGS="-march=broadwell"
+# export CXXFLAGS="-stdlib=libc++ -Wno-register -march=broadwell"
+# export FC=gfortran
+
+# export BLA_VENDOR=FlexiBLAS
+
+# # set up MKL / OpenMP:
+# export MKL_INTERFACE_LAYER=GNU,LP64
+# export MKL_THREADING_LAYER=SEQUENTIAL
+# export MKL_NUM_THREADS=1
+# export OMP_NUM_THREADS=1
+
+# # set number of threads for compiling and testing
+# NCORES=10
+
+# BUILDDIR=$(pwd)
+# # set installation directory (default pwd/install)
+# INSTALLDIR=$(pwd)/install
+
+# export TRIQS_ROOT=${INSTALLDIR}
+# export PATH=${INSTALLDIR}/bin:$PATH
+# export CPLUS_INCLUDE_PATH=${INSTALLDIR}/include:$CPLUS_INCLUDE_PATH
+# export LIBRARY_PATH=${INSTALLDIR}/lib:$LIBRARY_PATH
+# export LD_LIBRARY_PATH=${INSTALLDIR}/lib:$LD_LIBRARY_PATH
+# export PYTHONPATH=${INSTALLDIR}/lib/python3.10/site-packages:$PYTHONPATH
+# export CMAKE_PREFIX_PATH=${INSTALLDIR}/lib/cmake/triqs:$CMAKE_PREFIX_PATH
+# export CMAKE_PREFIX_PATH=${INSTALLDIR}/lib/cmake/cpp2py:$CMAKE_PREFIX_PATH
+# choose triqs + app to be installed
 export CC=clang
 export CXX=clang++
-export CFLAGS="-march=broadwell"
-export CXXFLAGS="-stdlib=libc++ -Wno-register -march=broadwell"
 export FC=gfortran
+#python version detection
+export PYVER=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+# compiler flags add stdlib=libc++ for clang
+export CXXFLAGS="-stdlib=libc++ -Wno-register -march=native"
 
-export BLA_VENDOR=FlexiBLAS
+# set blas / lapack Intel10_64_dyn | OpenBLAS | FlexiBLAS
+export BLA_VENDOR=Intel10_64_dyn
 
 # set up MKL / OpenMP:
 export MKL_INTERFACE_LAYER=GNU,LP64
@@ -30,10 +63,8 @@ export PATH=${INSTALLDIR}/bin:$PATH
 export CPLUS_INCLUDE_PATH=${INSTALLDIR}/include:$CPLUS_INCLUDE_PATH
 export LIBRARY_PATH=${INSTALLDIR}/lib:$LIBRARY_PATH
 export LD_LIBRARY_PATH=${INSTALLDIR}/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=${INSTALLDIR}/lib/python3.10/site-packages:$PYTHONPATH
-export CMAKE_PREFIX_PATH=${INSTALLDIR}/lib/cmake/triqs:$CMAKE_PREFIX_PATH
-export CMAKE_PREFIX_PATH=${INSTALLDIR}/lib/cmake/cpp2py:$CMAKE_PREFIX_PATH
-# choose triqs + app to be installed
+export PYTHONPATH=${INSTALLDIR}/lib/python${PYVER}/site-packages:$PYTHONPATH
+export CMAKE_PREFIX_PATH=${INSTALLDIR}/lib/cmake/triqs:${INSTALLDIR}/lib/cmake/cpp2py:$CMAKE_PREFIX_PATH
 packages="triqs tprf"
 
 
