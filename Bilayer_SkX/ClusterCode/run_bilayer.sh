cff9862abbdf4193545dfc0fe960871bc7fdca16
grovert4 <123444686+grovert4@users.noreply.github.com>
2024-05-01 18:30:14 -0400

    Update run_bilayer.sh

--- Bilayer_SkX/ClusterCode/run_bilayer.sh
+++ Bilayer_SkX/ClusterCode/run_bilayer.sh
@@ -10,6 +10,7 @@
 #SBATCH --time=18:00:00
 #SBATCH --mem-per-cpu=4000MB
 #SBATCH --output=/scratch/grovert4/SLURM/slurm-%x-%j.txt
-module load julia/1.9.3
+module load StdEnv/2020
+module load julia/1.8.5
 
 srun julia SkX_BiLayer_Run.jl 01.05.2024-Bilayer-decreasingfield
