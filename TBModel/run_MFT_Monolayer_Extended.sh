03628f44a9928b068fff4b75dfd85380a05c559f
andrewkhardy <andrewkoehlerhardy@gmail.com>
2024-05-08 22:33:06 -0400

    05.09

--- TBModel/run_MFT_Monolayer_Extended.sh
+++ TBModel/run_MFT_Monolayer_Extended.sh
@@ -15,5 +15,5 @@ module load StdEnv/2023
 module load julia/1.10.0
 module load intel/2023.2.1
 module load intelmpi
-mpirun julia --project=.. --heap-size-hint=1G MFT_wrapper_v2.jl 04.30.2024_Monolayer_NN ./Monolayer_MFT.jl
-mpirun julia --project=.. --heap-size-hint=1G Extract_LastData.jl "/scratch/a/aparamek/andykh/Data/Monolayer_Data" 04.30
+mpirun julia --project=.. --heap-size-hint=1G MFT_wrapper_v2.jl 05.10.2024_Monolayer_NN ./Monolayer_MFT.jl
+mpirun julia --project=.. --heap-size-hint=1G Extract_LastData.jl "/scratch/a/aparamek/andykh/Data/Monolayer_Data" 05.10
