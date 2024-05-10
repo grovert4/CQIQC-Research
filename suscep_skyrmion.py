7ccee5e622691b9b67e89580fe191aa762a828f9
Anjishnu Bose <anjishnubose98@gmail.com>
2024-05-10 14:16:10 -0400

    fixed skyrmion hoppings finally

--- suscep_skyrmion.py
+++ suscep_skyrmion.py
@@ -31,8 +31,8 @@ def positions(n1: int, n2:int, orbitals:int) -> list:
     pos = []
     names = []
     orbital_names = list(string.ascii_lowercase[0:orbitals])
-    for i in range(n1):
-        for j in range(n2):
+    for j in range(n2):
+        for i in range(n1):
             for orbital in range(orbitals):
                 pos.append((i*l1[0] + j*l2[0], i*l1[1] + j*l2[1], 0.0))
                 names.append(str(n2*i+j+1) + orbital_names[orbital])
@@ -53,9 +53,9 @@ def skyrmion():
                 "mid1" : B_mid1, "mid2" : B_mid2, "mid3" : B_mid3, 
                 "mid4" : B_mid4, "mid5" : B_mid5, "mid6" : B_mid6}
     
-    positions = {12 : "center", 
-                        1 : "edge", 8 : "edge", 9 : "edge", 10 : "edge", 4 : "edge",
-                        7 : "mid1", 6 : "mid2", 5 : "mid3", 11: "mid4", 2 : "mid5", 3 : "mid6"}
+    positions = {12 : "edge", 
+                        1 : "center", 10 : "mid6", 5 : "edge", 11 : "mid5", 8 : "mid3",
+                        4 : "edge", 9 : "edge", 3 : "edge", 6: "mid1", 7 : "mid2", 2 : "mid4"}
     
     mat = np.zeros((24, 24), dtype=np.complex128)
     
@@ -77,24 +77,25 @@ NN_00 = [(1, 2), (1, 7), (1, 8), (2, 3), (2, 8), (2, 9), (3, 4), (3, 9), (3, 10)
         (4, 5), (4, 10), (4, 11), (5, 6), (5, 11), (5, 12), 
         (6, 12), 
         (7, 8), (8, 9), (9, 10), (10, 11), (11, 12)] 
-##### between unit cell at (0, 1) : 11
-NN_01 = [(7, 3), (7, 4), (8, 4), (8, 5), (9, 5), (9, 6), (10, 6), (10, 1), (11, 1), (11, 2), (12, 2)]
-##### between unit cell at (1, 0) : 3
-NN_10 = [(6, 1), (6, 7), (12, 7)]
+##### between unit cell at (1, 0) : 7
+NN_10 = [(7, 3), (7, 4), (8, 4), (8, 5), (9, 5), (9, 6), (10, 6)]
+##### between unit cell at (0, 1) : 5
+NN_01 = [(10, 1), (11, 1), (11, 2), (12, 2), (12, 3)] 
 ##### between unit cell at (1, 1) : 1
-NN_11 = [(12, 3)]
+NN_1m1 = [(6, 1), (6, 7), (12, 7)]
 ##### all bonds
-bonds = {(0, 0) : NN_00, (0, 1) : NN_01, (1, 0) : NN_10, (1, 1) : NN_11}
+bonds = {(0, 0) : NN_00, (0, 1) : NN_01, (1, 0) : NN_10, (-1, 1) : NN_1m1}
 
+##### Fixed hopping parameters
 t1 = 1.0
-B = 1.0 * t1
-L = (6, 2)
+B = 4.0 * t1    ##### *Hund's coupling
+L = (6, 2)    ##### supercell size
 N = L[0] * L[1]
 
 pos, names = positions(*L, 2)
 ########## tight binding model #################
 model = TBLattice(
-    units = [(-3.0, np.sqrt(3), 0.0), (3.0, np.sqrt(3), 0.0)],##### Primitive vectors
+    units = [(-3.0, np.sqrt(3), 0.0), [3.0, np.sqrt(3), 0.0]],##### Primitive vectors
     orbital_positions = pos,##### 2 sublattice x 2 spins
     orbital_names = names,
     hoppings = {
@@ -104,8 +105,8 @@ model = TBLattice(
         (-1, 0): -t1 * np.conj(np.transpose(hopping(np.eye(2), bonds[(1, 0)]))),
         ( 0,+1): -t1 * hopping(np.eye(2), bonds[(0, 1)]),
         ( 0,-1): -t1 * np.conj(np.transpose(hopping(np.eye(2), bonds[(0, 1)]))),
-        ( 1, 1): -t1 * hopping(np.eye(2), bonds[(1, 1)]),
-        (-1,-1): -t1 * np.conj(np.transpose(hopping(np.eye(2), bonds[(1, 1)]))),
+        (-1, 1): -t1 * hopping(np.eye(2), bonds[(-1, 1)]),
+        ( 1,-1): -t1 * np.conj(np.transpose(hopping(np.eye(2), bonds[(-1, 1)]))),
 
         },
     )
@@ -133,8 +134,7 @@ plt.ylabel(r'$\epsilon(\mathbf{k})$')
 plt.grid(True)
 plt.savefig(f"band_structure_t1={t1}_B={B}.png")
 plt.close()
-# plt.show()
-# quit()
+
 ##### fermi distribution function
 def fermi(e, beta, mu):
     return 1.0 / (np.exp(beta * (e-mu)) + 1.0)
