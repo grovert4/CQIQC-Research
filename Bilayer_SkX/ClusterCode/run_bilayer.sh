b9b34c610e990c4a28c1c1a93b0ad54196937af4
grovert4 <123444686+grovert4@users.noreply.github.com>
2024-05-20 17:12:55 -0400

    Update run_bilayer.sh

--- Bilayer_SkX/ClusterCode/run_bilayer.sh
+++ Bilayer_SkX/ClusterCode/run_bilayer.sh
@@ -4,10 +4,10 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --ntasks=250
+#SBATCH --ntasks=300
 #SBATCH --cpus-per-task=1 
 #SBATCH --account=def-aparamek
-#SBATCH --time=18:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem-per-cpu=4000MB
 #SBATCH --output=/scratch/grovert4/SLURM/slurm-%x-%j.txt
 module load StdEnv/2020
