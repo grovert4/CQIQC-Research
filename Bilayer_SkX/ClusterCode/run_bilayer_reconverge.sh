8b4062c4a90e749f3510f1baa20f4f7f73410bf4
grovert4 <123444686+grovert4@users.noreply.github.com>
2024-05-22 23:39:43 -0400

    Create run_bilayer_reconverge.sh

---
+++ Bilayer_SkX/ClusterCode/run_bilayer_reconverge.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --mail-user=tanmay.grover@mail.utoronto.ca  
+#SBATCH --mail-type=ALL
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --ntasks=400
+#SBATCH --cpus-per-task=1 
+#SBATCH --account=def-aparamek
+#SBATCH --time=04:00:00
+#SBATCH --mem-per-cpu=4000MB
+#SBATCH --output=/scratch/grovert4/SLURM/slurm-%x-%j.txt
+module load StdEnv/2020
+module load julia/1.8.5
+
+srun julia Skx_Bilayer_Reconverge.jl 22-05-2024-Bilayer-reconverge
