55f28430b179dba1482034e62b585088bcbb5780
andrewkhardy <andrewkoehlerhardy@gmail.com>
2024-05-27 17:09:54 -0400

    data

--- TBModel/Plotting/monolayer_phase_diagram_v2.py
+++ TBModel/Plotting/monolayer_phase_diagram_v2.py
@@ -10,7 +10,7 @@ filename = "02.05.2024_Monolayer" # J = 4, N = 2
 #filename = "04.05.2024_Monolayer_E" # J = 4, N = 2
 #filename = "05.05.2024_Monolayer_4" # J = 4, N = 2
 
-filename = "02.05.2024_Monolayer_E"
+filename = "05.01-0.5.2024_Monolayer"
 
 
 os.chdir("/home/andrewhardy/Documents/Graduate/Codes/Skyrmion/TBModel/Plotting")
@@ -25,40 +25,39 @@ plt.rcParams.update({"text.usetex": True})
 params = yml.safe_load(Path(f"../Input/{filename}.yml").read_text())
 U_array = np.linspace(params["U_min"], params["U_max"], params["U_length"])
 filling_arr = np.linspace(params["filling_min"], params["filling_max"], params["filling_length"]) / (params["filling_max"]*2)
-Uniform_Status = False
-polarization = np.zeros((len(U_array), len(filling_arr)))
-energy = np.zeros((len(U_array), len(filling_arr)))
-conduct = np.zeros((len(U_array), len(filling_arr)))
-for (ind_f, filling) in enumerate(filling_arr):
+J_array = np.linspace(params["J_min"], params["J_max"], params["J_length"])
+polarization = np.zeros((len(U_array), len(J_array)))
+energy = np.zeros((len(U_array), len(J_array)))
+conduct = np.zeros((len(U_array), len(J_array)))
+for (ind_J, J) in enumerate(J_array):
     for (ind_u, U_var) in enumerate(U_array):
-        fileName = loc + f"Last_Itr_{filename}_p={round(filling, 3)}_U={round(U_var, 2)}_t1={round(t1, 2)}.jld2"
+        fileName = loc + f"Last_Itr_{filename}_J={round(J, 3)}_U={round(U_var, 2)}.jld2"
         TBResults = h5.File(fileName, 'r')
-        conduct[ind_u, ind_f] =  np.mean(TBResults["Chern Fill"])
-        energy[ind_u, ind_f] = TBResults["MFT_Energy"][-1]
-        if Uniform_Status == True:
-            polarization[ind_u, ind_f] = np.abs(TBResults["Expectations"][0])
-        else:
-            polarization[ind_u, ind_f] = np.mean(np.abs(TBResults["Expectations"][11]))
+        conduct[ind_u, ind_J] =  np.mean(TBResults["Chern Fill"])
+        energy[ind_u, ind_J] = TBResults["MFT_Energy"][-1]
+        temp = np.array(TBResults["ssf"])
+        ssf = temp['re'] + 1j*temp['im']
+        polarization[ind_u, ind_J] = np.abs(np.max(ssf))
 
-U_array_flat = U_array.repeat(len(filling_arr))
-filling_arr_flat = np.tile(filling_arr, len(U_array))
+U_array_flat = U_array.repeat(len(J_array))
+J_array_flat = np.tile(J_array, len(U_array))
 conduct_flat = conduct.flatten()
 polarization_flat = polarization.flatten()
 energy_flat = energy.flatten()
 # Create the scatter plot
-# plt.scatter(filling_arr_flat, U_array_flat,c=conduct_flat, cmap='viridis', vmax = 3)
+# plt.scatter(J_array_flat, U_array_flat,c=conduct_flat, cmap='viridis', vmax = 3)
 # plt.colorbar(label=r'$\sigma_{xy}$')
 # plt.ylabel(r'$U$')
 # plt.xlabel(r'$n$')
 
 # plt.show()
-# plt.scatter(filling_arr_flat, U_array_flat,c=polarization_flat, cmap='viridis')
+# plt.scatter(J_array_flat, U_array_flat,c=polarization_flat, cmap='viridis')
 # plt.colorbar(label=r'$P$')
 # plt.ylabel(r'$U$')
 # plt.xlabel(r'$n$')
 
 # plt.show()
-# plt.scatter(filling_arr_flat, U_array_flat,c=energy_flat, cmap='viridis')
+# plt.scatter(J_array_flat, U_array_flat,c=energy_flat, cmap='viridis')
 # plt.colorbar(label=r'$E$')
 # plt.ylabel(r'$U$')
 # plt.xlabel(r'$n$')
@@ -66,34 +65,34 @@ energy_flat = energy.flatten()
 # plt.show()
 
 # Uniform_Status = False
-# polarization = np.zeros((len(U_array), len(filling_arr)))
-# energy_2 = np.zeros((len(U_array), len(filling_arr)))
-# conduct = np.zeros((len(U_array), len(filling_arr)))
-# for (ind_f, filling) in enumerate(filling_arr):
+# polarization = np.zeros((len(U_array), len(J_array)))
+# energy_2 = np.zeros((len(U_array), len(J_array)))
+# conduct = np.zeros((len(U_array), len(J_array)))
+# for (ind_J, filling) in enumerate(J_array):
 #     for (ind_u, U_var) in enumerate(U_array):
 #         if Uniform_Status == True:
 #             fileName = loc + f"Last_Itr/Last_Itr_{filename}_UNIFORM_p={round(filling, 3)}_U={round(U_var, 2)}_t1={round(t1, 2)}.jld2"
 #         else:
 #             fileName = loc + f"Last_Itr/Last_Itr_{filename}_p={round(filling, 3)}_U={round(U_var, 2)}_t1={round(t1, 2)}.jld2"
 #         TBResults = h5.File(fileName, 'r')
-#         conduct[ind_u, ind_f] =  np.abs(TBResults["Chern Fill"])
-#         energy_2[ind_u, ind_f] = TBResults["MFT_Energy"][-1]
+#         conduct[ind_u, ind_J] =  np.abs(TBResults["Chern Fill"])
+#         energy_2[ind_u, ind_J] = TBResults["MFT_Energy"][-1]
 #         if Uniform_Status == True:
-#             polarization[ind_u, ind_f] = np.abs(TBResults["Expectations"][0])
+#             polarization[ind_u, ind_J] = np.abs(TBResults["Expectations"][0])
 #         else:
-#             polarization[ind_u, ind_f] = np.mean(np.abs(TBResults["Expectations"]))
+#             polarization[ind_u, ind_J] = np.mean(np.abs(TBResults["Expectations"]))
 
 
 conduct_flat = conduct.flatten()
 polarization_flat = polarization.flatten()
 # Create the scatter plot
-plt.scatter(filling_arr_flat, U_array_flat,c=conduct_flat, cmap='viridis', vmax =1 )
+plt.scatter(J_array_flat, U_array_flat,c=conduct_flat, cmap='viridis', vmax =1 )
 plt.colorbar(label=r'$\sigma_{xy}$')
 plt.ylabel(r'$U$')
 plt.xlabel(r'$n$')
 
 plt.show()
-plt.scatter(filling_arr_flat, U_array_flat,c=polarization_flat, cmap='viridis')
+plt.scatter(J_array_flat, U_array_flat,c=polarization_flat, cmap='viridis')
 plt.colorbar(label=r'$P$')
 plt.ylabel(r'$U$')
 plt.xlabel(r'$n$')
@@ -104,11 +103,11 @@ plt.show()
 fig = plt.figure(figsize=(8, 8))
 
 plt.imshow(np.round(conduct,3), aspect='auto', cmap='PRGn',vmin = -6, vmax=6, origin='lower',
-           extent=[filling_arr.min(), filling_arr.max(), U_array.min(), U_array.max()])
+           extent=[J_array.min(), J_array.max(), U_array.min(), U_array.max()])
 
 plt.colorbar(label=r'$\sigma_{xy}$')
 plt.ylabel(r'$U_1$')
-plt.xlabel(r'$n$')
+plt.xlabel(r'$J$')
 plt.ylim(U_array.min(), min(U_array.max(), 5))
 
 plt.savefig("Plots/Monolayer_Conductivity.pdf")
@@ -117,7 +116,7 @@ plt.show()
 fig = plt.figure(figsize=(8, 8))
 
 plt.imshow(polarization, aspect='auto', cmap='viridis', origin='lower',
-           extent=[filling_arr.min(), filling_arr.max(), U_array.min(), U_array.max()])
+           extent=[J_array.min(), J_array.max(), U_array.min(), U_array.max()])
 plt.colorbar(label=r'$P_1$')
 plt.ylabel(r'$U_1$')
 plt.xlabel(r'$n$')
