62c5607d179dfb3b36cbc863801b7cec529c9129
grovert4 <123444686+grovert4@users.noreply.github.com>
2024-05-30 17:23:43 -0400

    Update run_tempering_bilayer-J2=0.0.sh

--- Bilayer_SkX/ClusterCode/run_tempering_bilayer-J2=0.0.sh
+++ Bilayer_SkX/ClusterCode/run_tempering_bilayer-J2=0.0.sh
@@ -1,2 +1,16 @@
-#!/bin/bash\n#SBATCH --mail-user=tanmay.grover@mail.utoronto.ca  \n#SBATCH --mail-type=ALL\n#SBATCH --mail-type=BEGIN\n#SBATCH --mail-type=END\n#SBATCH --mail-type=FAIL\n#SBATCH --ntasks=20\n#SBATCH --cpus-per-task=1 \n#SBATCH 
---account=def-aparamek\n#SBATCH --time=12:00:00\n#SBATCH --mem-per-cpu=8000MB\n#SBATCH --output=/scratch/grovert4/SLURM/slurm-%x-%j.txt\nmodule load StdEnv/2020\nmodule load julia/1.8.5\n\nsrun julia SkX_Tempering_BiLayer_Run.jl J2=0.0-30.05.2024-Bilayer-tempering
+#!/bin/bash
+#SBATCH --mail-user=tanmay.grover@mail.utoronto.ca  
+#SBATCH --mail-type=ALL
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --ntasks=20
+#SBATCH --cpus-per-task=1 
+#SBATCH --account=def-aparamek
+#SBATCH --time=12:00:00
+#SBATCH --mem-per-cpu=8000MB
+#SBATCH --output=/scratch/grovert4/SLURM/slurm-%x-%j.txt
+
+module load StdEnv/2020
+module load julia/1.8.5
+srun julia SkX_Tempering_BiLayer_Run.jl J2=0.0-30.05.2024-Bilayer-tempering
