0594c9599bf80d179ecad23b99791a018bd56112
grovert4 <123444686+grovert4@users.noreply.github.com>
2024-05-30 17:10:49 -0400

    Update looper.sh

--- Bilayer_SkX/ClusterCode/looper.sh
+++ Bilayer_SkX/ClusterCode/looper.sh
@@ -3,7 +3,8 @@ string_to_search_for="run_tempering"
 current_dir=$(pwd)
 for FILE in $(ls $current_dir) 
 do
-	if [[ ${FILE} =~ ${string_to_search_for} ]]; then
+	if [[ ${FILE} =~ ${string_to_search_for} ]] 
+ 	then
 		echo ${FILE}
 		# sbatch -p ccq -C rome ${FILE}
 		sleep 1 # pause to be kind to the scheduler
