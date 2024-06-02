b1c81fa86c0bbdd412d2be8b3aca57c1179bbe2c
andrewkhardy <andrewkoehlerhardy@gmail.com>
2024-06-02 19:43:15 -0400

    data

--- TBModel/Bilayer_MFT_v2.jl
+++ TBModel/Bilayer_MFT_v2.jl
@@ -134,7 +134,7 @@ function MFT(params, filename)
             if haskey(params, "U_prev")
                 #oldfile = loc * "/$(filename)_p=$(round(filling, digits=3))_U=$(round(params["U_prev"], digits=2))_t1=$(round(t1, digits=2)).jld2"
                 #oldfile = loc * "/$(filename)_J=$(round(jh, digits=3))_U=$(round(params["U_prev"], digits=2)).jld2"
-                oldfile = loc * "/$(filename)_V=$(round(V, digits=3))_U=$(round(U, digits=2)).jld2"
+                oldfile = loc * "/$(filename)_V=$(round(V, digits=3))_U=$(round(params["U_prev"], digits=2)).jld2"
 
                 init_guess = load(oldfile)["outputs"][end] .+ vcat([0.0], [0.00000], rand_noise, -1 .* rand_noise)
                 SolveMFT!(mft, init_guess, fileName; max_iter=params["max_iter"], tol=params["tol"])
@@ -146,7 +146,7 @@ function MFT(params, filename)
         if haskey(params, "U_prev")
             #oldfile = loc * "/$(filename)_p=$(round(filling, digits=3))_U=$(round(params["U_prev"], digits=2))_t1=$(round(t1, digits=2)).jld2"
             #oldfile = loc * "/$(filename)_J=$(round(jh, digits=3))_U=$(round(params["U_prev"], digits=2)).jld2"
-            oldfile = loc * "/$(filename)_V=$(round(V, digits=3))_U=$(round(U, digits=2)).jld2"
+            oldfile = loc * "/$(filename)_V=$(round(V, digits=3))_U=$(round(params["U_prev"], digits=2)).jld2"
 
             init_guess = load(oldfile)["outputs"][end] .+ vcat([0.0000000000], [0.0000], rand_noise, -1 .* rand_noise)
             SolveMFT!(mft, init_guess, fileName; max_iter=params["max_iter"], tol=params["tol"])
