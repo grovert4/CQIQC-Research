4bd527fd53abeacb6efcecbaf5a548991f2e90b2
grovert4 <123444686+grovert4@users.noreply.github.com>
2024-06-12 12:51:20 -0400

    Update run_epsilon0_bilayer.sh

--- Bilayer_SkX/ClusterCode/run_epsilon0_bilayer.sh
+++ Bilayer_SkX/ClusterCode/run_epsilon0_bilayer.sh
@@ -7,10 +7,10 @@
 #SBATCH --ntasks=20
 #SBATCH --cpus-per-task=1 
 #SBATCH --account=def-aparamek
-#SBATCH --time=10:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem-per-cpu=8000MB
 #SBATCH --output=/scratch/grovert4/SLURM/slurm-%x-%j.txt
 module load StdEnv/2020
 module load julia/1.8.5
 
-srun julia SkX_Epsilon_BiLayer_Run.jl 10.06.2024-Bilayer-Epsilon_1.0-Tempering
+srun julia SkX_Epsilon_BiLayer_Run.jl 10.06.2024-Bilayer-Epsilon_0.8-Tempering
