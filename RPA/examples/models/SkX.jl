a91cd66b48f034129627c149ec6be99a33660964
Andrew Hardy <andrewhardy@pop-os.localdomain>
2024-07-08 15:46:32 -0400

    intermat

--- RPA/examples/models/SkX.jl
+++ RPA/examples/models/SkX.jl
@@ -63,4 +63,4 @@ CreateUnitCell!(UC, HoppingParams)
 #####* Saving the unit cell in a JLD2 file
 file_name = "/scratch/a/aparamek/andykh/Data/Monolayer_Data/RPA/SkX.jld2"
 # change to scratch
-save(file_name, Dict("unit cell" => UC, "parameters" => Hoppingparams))
+save(file_name, Dict("unit cell" => UC, "parameters" => HoppingParams))
