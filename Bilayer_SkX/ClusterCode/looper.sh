ab2220cbfdafcfb2ce71d4fdd87aa5e6e3db2fd0
grovert4 <123444686+grovert4@users.noreply.github.com>
2024-07-09 19:07:01 -0400

    Update looper.sh

--- Bilayer_SkX/ClusterCode/looper.sh
+++ Bilayer_SkX/ClusterCode/looper.sh
@@ -5,6 +5,7 @@ for FILE in $(ls $current_dir)
 do
 	if [[ ${FILE} =~ ${string_to_search_for} ]] 
  	then
+  		dos2unix ${FILE}
 		echo ${FILE}
 		sbatch ${FILE}
 		sleep 1 # pause to be kind to the scheduler
