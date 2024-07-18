2245b1373f1073fdc1f1ebbb2e8ae4c7b94ff114
Andrew Hardy <andrewhardy@pop-os.localdomain>
2024-07-18 09:11:13 -0400

    wtf

--- RPA/src/SkX_RPA.sh
+++ RPA/src/SkX_RPA.sh
@@ -15,12 +15,12 @@ module load StdEnv/2023
 module load gcc/12 openmpi
 module load python/3.11 imkl cmake clang mpi4py hdf5 boost fftw
 source ~/triqsvenv/bin/activate
-julia --project=../../TBModel ../examples/models/SkX_0.jl
+julia --project=../../TBModel ../examples/models/SkX_Coulomb_0.jl
 julia --project=../../TBModel ../examples/interactions/SkX_Coulomb_0.jl
 julia --project=../../TBModel run_RPA.jl --input ../Inputs/07.17-0.2024_SkX.yml --run_bare true
-# julia --project=../../TBModel ../examples/models/SkX_2.jl
-# julia --project=../../TBModel ../examples/interactions/SkX_Coulomb_2.jl
-# julia --project=../../TBModel run_RPA.jl --input ../Inputs/07.17-2.2024_SkX.yml --run_bare true
-# julia --project=../../TBModel ../examples/models/SkX_4.jl
-# julia --project=../../TBModel ../examples/interactions/SkX_Coulomb_4.jl
-# julia --project=../../TBModel run_RPA.jl --input ../Inputs/07.17-4.2024_SkX.yml --run_bare true
+julia --project=../../TBModel ../examples/models/SkX_Coulomb_2.jl
+julia --project=../../TBModel ../examples/interactions/SkX_Coulomb_2.jl
+julia --project=../../TBModel run_RPA.jl --input ../Inputs/07.17-2.2024_SkX.yml --run_bare true
+julia --project=../../TBModel ../examples/models/SkX_Coulomb_4.jl
+julia --project=../../TBModel ../examples/interactions/SkX_Coulomb_4.jl
+julia --project=../../TBModel run_RPA.jl --input ../Inputs/07.17-4.2024_SkX.yml --run_bare true
