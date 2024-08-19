e1a3f1ecc698cd5f81401dcec4169353753cbd74
grovert4 <123444686+grovert4@users.noreply.github.com>
2024-08-19 15:39:31 -0400

    Update run_monolayer.sh

--- Bilayer_SkX/ClusterCode/run_monolayer.sh
+++ Bilayer_SkX/ClusterCode/run_monolayer.sh
@@ -7,8 +7,8 @@
 #SBATCH --ntasks=300
 #SBATCH --cpus-per-task=1 
 #SBATCH --account=def-aparamek
-#SBATCH --time=10:00:00
-#SBATCH --mem-per-cpu=4000MB
+#SBATCH --time=06:00:00
+#SBATCH --mem-per-cpu=8000MB
 #SBATCH --output=/scratch/grovert4/SLURM/slurm-%x-%j.txt
 module load julia/1.8.5
 
