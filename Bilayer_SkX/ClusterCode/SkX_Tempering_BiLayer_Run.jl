9e72eba3082f08a66511f65978f9ceeec9227587
grovert4 <123444686+grovert4@users.noreply.github.com>
2024-09-05 15:21:42 -0400

    Update SkX_Tempering_BiLayer_Run.jl

--- Bilayer_SkX/ClusterCode/SkX_Tempering_BiLayer_Run.jl
+++ Bilayer_SkX/ClusterCode/SkX_Tempering_BiLayer_Run.jl
@@ -65,7 +65,7 @@ j2 = inputFile["J2"]
 for j in 1:length(Jperps)
       jperp = round(Jperps[j],sigdigits=5)
 
-      filename = "/scratch/grovert4/Data/Aminus-Bi/"*ARGS[1]*"_Jperp=$(jperp),J2=$(j2).h5"
+      filename = "/scratch/grovert4/Data/Aminus-Full-Bilayer/"*ARGS[1]*"_Jperp=$(jperp),J2=$(j2).h5"
       if isfile(filename) 
          println("Already Completed "*filename)
       else
